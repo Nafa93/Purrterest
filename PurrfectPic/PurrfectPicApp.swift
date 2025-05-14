7d8113d98daccd748be84e1b4fc9a09088b76de4
Nicolas Alejandro Fernandez Amorosino <nicolasfernandez93@gmail.com>
2025-05-14 20:13:05 -0300

    Initial Commit

---
+++ PurrfectPic/PurrfectPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurrfectPicApp.swift
+//  PurrfectPic
+//
+//  Created by Nicolas Alejandro Fernandez Amorosino on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct PurrfectPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
